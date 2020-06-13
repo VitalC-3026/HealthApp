be2dc5b0f04de1b4a9129225a882af1d8744eeae
马生鸿 <1811404@mail.nankai.edu.cn>
2020-06-13 14:03:09 +0800

    修改记录使用手机姿势错误时间的方式，返回错误使用手机姿势时间 增加移动时使用手机的判断，返回移动时使用手机的时间

--- app/src/main/java/com/example/bigproject/orientation/OrientSensor.java
+++ app/src/main/java/com/example/bigproject/orientation/OrientSensor.java
@@ -8,7 +8,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
 public class OrientSensor implements SensorEventListener {
-    private long totalTime;
+    private float totalTime=0;
     private long lastTime;
     private float[] values, r, accelerometer, magnetic;
     private SensorManager sensorManager;
@@ -20,10 +20,14 @@ public class OrientSensor implements SensorEventListener {
         this.context=context;
         this.orientCallBack=orientCallBack;
         this.configuration=context.getResources().getConfiguration();
+        values=new float[3];
+        r=new float[9];
+        accelerometer=new float[3];
+        magnetic=new float[3];
     }
 
     public interface OrientCallBack{
-        void getTime(long time);
+        void getOrientTime(float time);
     }
 
      /**
@@ -32,9 +36,9 @@ public class OrientSensor implements SensorEventListener {
     public boolean registerOrient(){
         sensorManager= (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
         boolean isAvailable = true;
-        if(!sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_UI))
+        if(!sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME))
             isAvailable=false;
-        if(!sensorManager.registerListener(this,sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),SensorManager.SENSOR_DELAY_UI))
+        if(!sensorManager.registerListener(this,sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),SensorManager.SENSOR_DELAY_GAME))
             isAvailable=false;
         return  isAvailable;
     }
@@ -49,36 +53,33 @@ public class OrientSensor implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent event) {
         long currentTime=System.currentTimeMillis();
-        if(currentTime-lastTime<500) return;
+        if(currentTime-lastTime<100) return;
         lastTime=currentTime;
         if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER) accelerometer = event.values.clone();
         if(event.sensor.getType()==Sensor.TYPE_MAGNETIC_FIELD) magnetic = event.values.clone();
 
         SensorManager.getRotationMatrix(r,null,accelerometer,magnetic);
         SensorManager.getOrientation(r,values);
-        //float azimuth = (float) Math.toDegrees(values[0]);
         float pitch = (float) Math.toDegrees(values[1]);
         float roll = (float) Math.toDegrees(values[2]);
 
         boolean flag=false;
         if(configuration.orientation==Configuration.ORIENTATION_LANDSCAPE) {
-            if (pitch > -50 && pitch < 50 && roll > -5 && roll < 80)
+            if (pitch > -30 && pitch < 30 && roll < 80 && roll > -80)
                 flag = true;
         }else {
-            if(pitch > -80 && pitch < 5 && roll > -30 && roll < 30)
+            if(pitch > -90 && pitch < 5 && roll > -30 && roll < 30)
                 flag=true;
         }
         if(!flag){
-            lastTime=System.currentTimeMillis();
-            totalTime+=500;
+            totalTime+=0.1;
+            orientCallBack.getOrientTime(totalTime);
         }
-        orientCallBack.getTime(totalTime);
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
-
 
 }
